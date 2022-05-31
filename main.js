40e59badaa654edfd81bc37efafa280ce7c91ef1
dkdkwl <dkdkwl@naver.com>
2022-06-01 01:54:48 +0900

    똑같은 오휴 사이트터짐

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // 유저가 값을 입력한다. ok
 // +버튼을 클릭하면, 할일이 추가된다. none
-// delete버튼을 누르면 할일이 삭제된다.
-// check버튼을 누르면 할일이 끝나면서 밑줄이 간다.
-// 1. check 버튼을 클릭하는 순간 true 
-// 2. true이면 끝난걸로 간주하고 밑줄 보여주기 
-// 3. false 이면 안끄ㅌ난걸로 알고 간주하고 보야주기  
+// delete버튼을 누르면 할일이 삭제된다. ok 
+// check버튼을 누르면 할일이 끝나면서 밑줄이 간다.ok none
+// 1. check 버튼을 클릭하는 순간 true ok
+// 2. true이면 끝난걸로 간주하고 밑줄 보여주기 none
+// 3. false 이면 안끄ㅌ난걸로 알고 간주하고 보야주기  ok
 // 진행중 끝남 탭을 누르면, 언더바가 이동한다. 
 // 끝남탭은, 끝난아이템만, 진행중탭은 진행중인 아이템만 
 // 전체탭을 누르면 다시 전체아이템으로 돌아옴
@@ -13,9 +13,16 @@
 let taskInput = document.querySelector(".task-input");
 let buttonAdd = document.querySelector(".button-add");
 let taskBoard = document.querySelector("#task-board");
+let tabType = document.querySelectorAll(".tab-type > div");
 let taskList = [];
+let completeList = [];
 
-buttonAdd.addEventListener("click",taskAdd)
+buttonAdd.addEventListener("click",taskAdd);
+for(let i = 0; i < tabType.length; i++){
+    tabType[i].addEventListener("click",(e)=>{
+        tabSort(e);
+    });
+}
 
 function taskAdd(){
     let userValue = taskInput.value;
@@ -32,7 +39,6 @@ function render(){
     let taskContent = "";
 
     for(let i = 0; i < taskList.length; i++){
-
         if(taskList[i].isComplete == true){
             taskContent += `<div class="task task-done">
                 <span>${taskList[i].content}</span>
@@ -73,9 +79,24 @@ function taskCheck(id){
     render();
 }
 
+function tabSort(e){
+    let currentId = e.currentTarget.id;
+    if(currentId == "tab-all"){
+        render();
+    }else if(currentId == "tab-not-done"){
+        for(let i = 0; i < taskList.length; i++){
+            if(taskList[i].isComplete == true){
+                completeList.push(taskList[i])
+            }
+        }
+        console.log(completeList)
+        taskList = completeList;
+    }
+    render();
+}
+
+
 function randomIDGenerate(){
     return "_" + Math.random().toString(36).substr(2, 9);
 }
-
-    
-    
+   
